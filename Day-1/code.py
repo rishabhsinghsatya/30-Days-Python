0dab87691316f6d90721015eb13557b229e96530
Rishabh Singh <vineetsatya36@gmail.com>
2024-12-20 01:49:17 +0530

    print and variables, input

---
+++ Day-1/code.py
@@ -0,0 +1,11 @@
+# print statements
+
+print("Hello world")
+
+a = 10
+b = 20
+c = a + b
+print(c)
+
+name = "python"
+print("Programming language is " + name)
---
+++ Day-2/code.py
@@ -0,0 +1,17 @@
+name = "Alice"
+age = 25
+height = 5.6
+is_student = True
+
+# Use type() to check the type of a variable
+print(type(name))
+print(type(age))
+
+# Use the input() function to accept input from the user
+name = input("Enter name: ")
+print("Programming language is " + name)
+
+# Convert string input into integers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print("Sum:", num1 + num2)
