43c24e70d80e86019530774e323a8342fd63b235
Rishabh Singh <vineetsatya36@gmail.com>
2024-12-29 01:48:35 +0530

    Modules and Packages

---
+++ Day-12/code-2/code.py
@@ -0,0 +1,12 @@
+# Importing Built-in Modules
+import math
+import os
+
+# Math module usage
+circle_radius = 5
+circle_area = math.pi * math.pow(circle_radius, 2)
+print("Circle Area:", circle_area)
+
+# OS module usage
+current_directory = os.getcwd()
+print("Current Directory:", current_directory)
